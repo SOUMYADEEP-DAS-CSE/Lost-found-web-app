9a2a288a27b4f47829d3bb316e3adb7ab95b43d6
Darken-rider07 <samyabrata012@gmail.com>
2025-07-20 16:27:17 +0530

    all done

--- src/components/LostItemForm.tsx
+++ src/components/LostItemForm.tsx
@@ -203,7 +203,7 @@ export default function LostAndFoundPage() {
         <motion.div layout className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {items.map((item) => (
             <motion.div
-              key={item._id ?? `temp-${item.id}`}
+              key={item._id || `local-${item.id}`} 
               layout
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
